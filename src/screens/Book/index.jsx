461a738ed3bb8cd2b04742bf023e144489b0d585
Krishna Mohan <krishnam@geekyants.com>
2021-01-25 21:25:34 +0530

    bookstore v3.1 -bug-fix -removed console.log() from book page

--- src/screens/Book/index.jsx
+++ src/screens/Book/index.jsx
@@ -8,7 +8,6 @@ import { useDispatch, useSelector } from "react-redux";
 export default function Book(props) {
   const dispatch = useDispatch();
   const newBooks = useSelector((state) => state.newBooks);
-  console.log(newBooks);
   const comics = useSelector((state) => state.comics);
   const loader = useSelector((state) => state.loader.data);
   useEffect(() => {
