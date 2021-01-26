c9fa8880281604b6dc77fb8a6311a4053e437b4d
Krishna Mohan <krishnam@geekyants.com>
2021-01-27 00:00:22 +0530

    booksotre v3.1

--- src/utility/forms/addPhoneForm.jsx
+++ src/utility/forms/addPhoneForm.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPhone } from "../../actions/authActions";
 export default function AddAddressForm() {
+  const userPhone = useSelector((state) => state.auth.user.phone);
   const dispatch = useDispatch();
-  const [phone, setPhone] = useState("");
+  const [phone, setPhone] = useState(userPhone);
   function handleChange(e) {
     setPhone(e.target.value);
   }
@@ -27,6 +28,7 @@ export default function AddAddressForm() {
         variant="outlined"
         onChange={handleChange}
         type="text"
+        value={phone}
       />
       <br />
       <Button variant="contained" color="primary" type="submit">
